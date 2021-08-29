a6144b02835673f44688f2c859ece008a5904e78
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:27:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OverloadingMethod OM = new OverloadingMethod();
+            OM.add(20, 30);
+            OM.add(20, 30, 40);
+            OM.add(20, 15.7f);
+            OM.add(20.5f, 30.5f);
+        }
+    }
+    public class OverloadingMethod
+    {
+        public void add(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("1st Sum is :" + sum);
+        }
+        public void add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            Console.WriteLine("2nd Sum is :" + sum);
+        }
+        public void add(int a, float b)
+        {
+            float sum = a + b;
+            Console.WriteLine("2rd Sum is :" + sum);
+        }
+        public void add(float a, float b)
+        {
+            float sum = a + b;
+            Console.WriteLine("4th Sum is :" + sum);
+        }
+    }
+}
